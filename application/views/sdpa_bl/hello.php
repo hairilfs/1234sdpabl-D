01ed3bbf3cff3180013164d6a77937ddc5e5a457
hairilfs <hairilfiqri@gmail.com>
2015-08-13 11:25:10 +0700

    Revert "test upload"
    This reverts commit 3a44c8b82e9dcb5dfb9d261fb1065eaf8faf07fd.

--- application/views/sdpa_bl/hello.php
+++
@@ -1,4 +0,0 @@
-<div class="jumbotron">
-    <h1>Hello, Admin!</h1>
-    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-</div>
